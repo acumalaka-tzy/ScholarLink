a4df80cbd4c0e058b78e7032ded3901f53cadaf9
roelyaaldi-sudo <roelyaaldi@gmail.com>
2026-05-12 22:28:57 +0700

    Add files via upload

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,25 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    protected $table = 'documents';
-
-    protected $primaryKey = 'id_dokumen';
-
-    protected $fillable = [
-        'id_application',
-        'jenis_dokumen',
-        'nama_file',
-        'file_path',
-        'tanggal_upload'
-    ];
-
-    // Relasi ke application
     public function application()
     {
-        return $this->belongsTo(
-            Application::class,
-            'id_application',
-            'id_application'
-        );
+        return $this->belongsTo(Application::class, 'id_application');
     }
 }
