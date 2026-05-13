4434b79e0426e936408e6eb2b853f62c59a5d13b
NaomiMS-seni <mtrsmmr@gmail.com>
2026-05-13 09:39:40 +0700

    Menambahkan relasi Profile ke User

--- database/migrations/2026_05_09_091036_create_profiles_table.php
+++ database/migrations/2026_05_09_091036_create_profiles_table.php
@@ -6,22 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('profiles');
+        Schema::table('profiles', function (Blueprint $table) {
+
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+
+        });
     }
 };
