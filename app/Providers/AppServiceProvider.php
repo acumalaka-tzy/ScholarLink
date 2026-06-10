54968c9c37271f09def82c937c5d97f3c8568638
gabrielllll <gabrielparhusip59@gmail.com>
2026-06-11 02:16:23 +0700

    Fix mailtrap registration and add symfony http client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,11 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        \Illuminate\Support\Facades\Mail::extend('mailtrap', function (array $config) {
+            $factory = new \Symfony\Component\Mailer\Bridge\Mailtrap\Transport\MailtrapTransportFactory();
+            $token = env('MAILTRAP_TOKEN', '44cd62ab0d6cda7fc558af6cc1166687');
+            return $factory->create(new \Symfony\Component\Mailer\Transport\Dsn('mailtrap+api', 'default', $token));
+        });
     }
 }
