06aed4a989bd03b3cf5676036d68ca1bbae88619
gabrielllll <gabrielparhusip59@gmail.com>
2026-06-10 11:40:26 +0700

    membuat bisa ditampilin ke ngrok

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware) {
 
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'provider' => \App\Http\Middleware\ProviderMiddleware::class,
